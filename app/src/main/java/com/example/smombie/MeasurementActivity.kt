3d7276c025c4d4041d04f27f1532cd50293b9279
mahiro <mahiro.1192@gmail.com>
2021-02-03 17:36:33 +0900

    mod : csvをopenCSVで読む処理の修正 add : 計測画面のボタン追加

--- app/src/main/java/com/example/smombie/MeasurementActivity.kt
+++ app/src/main/java/com/example/smombie/MeasurementActivity.kt
@@ -6,41 +6,41 @@ import android.util.Log
 import com.opencsv.CSVIterator
 import com.opencsv.CSVReader
 import com.opencsv.bean.CsvToBeanBuilder
-import java.io.BufferedReader
-import java.io.FileReader
-import java.io.InputStreamReader
-import java.io.StringReader
+import java.io.*
 
 class MeasurementActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
-    private val questionList = questionReader()
+    private lateinit var questionList : MutableList<Question>   //出題する問題のリスト
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_measurement)
+
+        questionList = questionReader(getAssets().open("test.csv"))
+
         Log.d(TAG,Metadata.getLabel()+"2")
         
-        Log.d(TAG, "" +questionReader())
+        Log.d(TAG, "" + questionList)
     }
 
-    fun questionReader():MutableList<Question>{
-            var questionList = mutableListOf<Question>()
-            try{
-                val csvStream = BufferedReader(InputStreamReader(getAssets().open("test.csv")))
-                val beanList = CsvToBeanBuilder<Question>(csvStream)
-                    .withType(Question::class.java)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build()
-                    .parse()
+    //csvから問題を読み取り，リストにする
+    private fun questionReader(fileName : InputStream):MutableList<Question>{
+        var questionList = mutableListOf<Question>()
+        try{
+            val csvStream = BufferedReader(InputStreamReader(fileName))
+            val beanList = CsvToBeanBuilder<Question>(csvStream)
+                .withType(Question::class.java)
+                .build()
+                .parse()
 
-                for (q in beanList) {
-                    questionList.add(q)
-                }
-            }catch(e :Exception){
-                Log.d(TAG,"e.toString()")
+            for (q in beanList) {
+                questionList.add(q)
             }
-            return questionList
+        }catch(e :Exception){
+            Log.d(TAG,"ファイルがないよ！！")
         }
+        return questionList
+    }
 
 
 }
