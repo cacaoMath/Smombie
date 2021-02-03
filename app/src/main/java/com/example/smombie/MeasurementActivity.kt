593e9770dd022a31c53ca8417981675c5b366183
mahiro <mahiro.1192@gmail.com>
2021-02-03 16:11:59 +0900

    csvをopenCSVで読む処理を追加，それ用にQuestion DataClass生成

--- app/src/main/java/com/example/smombie/MeasurementActivity.kt
+++ app/src/main/java/com/example/smombie/MeasurementActivity.kt
@@ -5,16 +5,42 @@ import android.os.Bundle
 import android.util.Log
 import com.opencsv.CSVIterator
 import com.opencsv.CSVReader
+import com.opencsv.bean.CsvToBeanBuilder
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.InputStreamReader
 import java.io.StringReader
 
 class MeasurementActivity : AppCompatActivity() {
     private val TAG = this::class.java.simpleName
+    private val questionList = questionReader()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_measurement)
         Log.d(TAG,Metadata.getLabel()+"2")
+        
+        Log.d(TAG, "" +questionReader())
     }
+
+    fun questionReader():MutableList<Question>{
+            var questionList = mutableListOf<Question>()
+            try{
+                val csvStream = BufferedReader(InputStreamReader(getAssets().open("test.csv")))
+                val beanList = CsvToBeanBuilder<Question>(csvStream)
+                    .withType(Question::class.java)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build()
+                    .parse()
+
+                for (q in beanList) {
+                    questionList.add(q)
+                }
+            }catch(e :Exception){
+                Log.d(TAG,"e.toString()")
+            }
+            return questionList
+        }
 
 
 }
--- app/src/main/java/com/example/smombie/Question.kt
+++ app/src/main/java/com/example/smombie/Question.kt
@@ -1,3 +1,24 @@
 package com.example.smombie
 
-data class Question()
+import com.opencsv.bean.CsvBindByName
+
+data class Question(
+    @CsvBindByName(column = "問題番号", required = true)
+    val num: String = "",
+
+    @CsvBindByName(column = "問題文", required = true)
+    val question: String = "",
+
+    @CsvBindByName(column = "正解", required = true)
+    val collectAns: String = "",
+
+    @CsvBindByName(column = "回答1", required = true)
+    val otherAns1: String = "",
+
+    @CsvBindByName(column = "回答2", required = true)
+    val otherAns2: String = "",
+
+    @CsvBindByName(column = "回答3", required = true)
+    val otherAns3: String = ""
+
+)
