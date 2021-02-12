34afd8ee83c96cc3aa6db5c0fb641f1bc2d4d058
mahiro <mahiro.1192@gmail.com>
2021-02-12 10:41:15 +0900

    mod : settingActivityと.xmlの変更

--- app/src/main/java/com/example/smombie/SettingActivity.kt
+++ app/src/main/java/com/example/smombie/SettingActivity.kt
@@ -1,11 +1,24 @@
 package com.example.smombie
 
+import android.animation.ObjectAnimator
+import android.graphics.Path
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.animation.PathInterpolator
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) : Boolean {
+        if (item.itemId == android.R.id.home){
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
